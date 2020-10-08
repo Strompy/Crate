36ea49082b1883de571724fbe058cf14e2c78393
Rachel Williams <rachelwilliams659@gmail.com>
2020-10-08 16:06:17 -0600

    Add annotations to user.js routes file

--- code/web/src/setup/routes/user.js
+++ code/web/src/setup/routes/user.js
@@ -1,10 +1,11 @@
-// App Imports
+// App Imports for components that represent pages on app
 import Login from '../../modules/user/Login'
 import Signup from '../../modules/user/Signup'
 import Profile from '../../modules/user/Profile'
 import Subscriptions from '../../modules/user/Subscriptions'
 
-// User routes
+// User routes object 
+//each property in object has a key of the route name and a value with holds the path of that route and the component it should display 
 export default {
   login: {
     path: '/user/login',
@@ -16,6 +17,7 @@ export default {
     component: Signup
   },
 
+  //for the following 2 route properties, auth is true, meaning these are "private" pages that can only be viewed when logged in 
   profile: {
     path: '/user/profile',
     component: Profile,

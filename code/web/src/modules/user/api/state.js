b96ea24cc46e20bcd1120a6bad1c01e71b041df5
Rachel Williams <rachelwilliams659@gmail.com>
2020-10-08 17:26:30 -0600

    Add annotations for user state reducer

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -1,8 +1,8 @@
-// App Imports
+// App Imports for helper function to check if object is empty, as well as all relevant actions that could be dispatched to reducer 
 import { isEmpty } from '../../../setup/helpers'
 import { SET_USER, LOGIN_REQUEST, LOGIN_RESPONSE, LOGOUT } from './actions'
 
-// Initial State
+// Initial State relating to user data on app load; details is an object with name, email & role properties 
 export const userInitialState = {
   error: null,
   isLoading: false,
@@ -10,9 +10,11 @@ export const userInitialState = {
   details: null
 }
 
-// State
+// Reducer relating to the "user" state in Redux store 
 export default (state = userInitialState, action) => {
   switch (action.type) {
+    //if action type is SET_USER, a new copy of user state is returned that updates user details with the user object passed in
+    //It also checks if user object is empty & if not, isAuthenticated becomes true 
     case SET_USER:
       return {
         ...state,
@@ -20,6 +22,7 @@ export default (state = userInitialState, action) => {
         details: action.user,
       }
 
+    //if action type is LOGIN_REQUEST, error property is reset to null & isLoading is set to true 
     case LOGIN_REQUEST:
       return {
         ...state,
@@ -27,6 +30,7 @@ export default (state = userInitialState, action) => {
         isLoading: action.isLoading
       }
 
+    //if action type is LOGIN_RESPONSE, error property is set to whatever is specified in action & isLoading is updated to false
     case LOGIN_RESPONSE:
       return {
         ...state,
@@ -34,6 +38,7 @@ export default (state = userInitialState, action) => {
         isLoading: false
       }
 
+    //if action type is LOGOUT, all user data is reset to null/false
     case LOGOUT:
       return {
         ...state,
@@ -43,6 +48,7 @@ export default (state = userInitialState, action) => {
         details: null
       }
 
+    //if action type does not match the above or isn't passed in, state stays the same
     default:
       return state
   }

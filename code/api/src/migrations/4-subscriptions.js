2aae2f346967ed3ef119b9e2463c775946366099
Sienna Kopf <princess.kopf@gmail.com>
2020-10-09 07:39:49 -0600

    Add annotations to /src/migrations/4-subscriptions.js discussing relational database tables and foreign keys

--- code/api/src/migrations/4-subscriptions.js
+++ code/api/src/migrations/4-subscriptions.js
@@ -7,14 +7,21 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      // foreign key alternate table connections
+      // subsciptions 'belong to' a user, users 'have many' subscriptions
+      // naming conventional (camel case) but principally the same as RoR
       userId: {
+        // explicitly specifies integer value
         type: Sequelize.INTEGER,
+        // demonstrates reference to a specific model and key
+        // more granular control than RoR
         references: {
           model: 'users',
           key: 'id'
         },
         allowNull: false
       },
+      // one to many -- crate to subscriptions
       crateId: {
         type: Sequelize.INTEGER,
         references: {
@@ -37,3 +44,6 @@ module.exports = {
     return queryInterface.dropTable('subscriptions');
   }
 }
+
+// more relationships might need to be added to subscriptions depending on how deliveries are handled
+// strong reminicience to RoR shouldnt make forming relational DB tables too hard

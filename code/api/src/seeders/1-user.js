d6ae8f6444dc41afe3e62e2bb6bfd2a37643d461
Sienna Kopf <princess.kopf@gmail.com>
2020-10-10 10:57:33 -0600

    Annotate api/seeders/user.js for fields that need or are optional to add to User.

--- code/api/src/seeders/1-user.js
+++ code/api/src/seeders/1-user.js
@@ -10,6 +10,10 @@ module.exports = {
       {
         name: 'The Admin',
         email: 'admin@crate.com',
+        // shipping address muct be added.
+          // image and description can be optional adds.
+          // having at least one user who has an image and description,
+          // and one that needs to add those fields would be good
         password: bcrypt.hashSync('123456', config.saltRounds),
         role: params.user.roles.admin,
         createdAt: new Date(),
@@ -18,6 +22,7 @@ module.exports = {
       {
         name: 'The User',
         email: 'user@crate.com',
+        // shipping address must be added. 
         password: bcrypt.hashSync('123456', config.saltRounds),
         role: params.user.roles.user,
         createdAt: new Date(),

c7bd8a792a328cd47876f25d43c27e535b96b7ed
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-10 23:00:51 -0600

    High level annotation for grid and gridcell style components #37

--- code/web/src/ui/grid/index.js
+++ code/web/src/ui/grid/index.js
@@ -1,4 +1,4 @@
-import Grid from './Grid'
-import GridCell from './GridCell'
+import Grid from './Grid' // Creates flex container for grid styling
+import GridCell from './GridCell' // Creates appropriately-styled child element for the Grid container
 
 export { Grid, GridCell }

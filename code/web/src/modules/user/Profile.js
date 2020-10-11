b11a6fb951b0d3b4065a992799b8b8b78de76e46
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-10 23:21:01 -0600

    Annotate component and proptypes #37

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -6,7 +6,7 @@ import { Helmet } from 'react-helmet' // manages changes to the document head (m
 import { Link } from 'react-router-dom' // Link turns an element into a link for React-Router <---
 
 // UI Imports
-import { Grid, GridCell } from '../../ui/grid' // Theses are components that give elements flex styling <---
+import { Grid, GridCell } from '../../ui/grid' // Theses are style components that give elements flex-based grid styling <---
 import { H3, H4 } from '../../ui/typography'// Imports conditional styling components for H1 and H2 <---
 import Button from '../../ui/button' // Imports Button component with conditional styling <---
 import { grey, grey2 } from '../../ui/common/colors' // Imports color variables from colors sheet <---
@@ -20,36 +20,36 @@ const Profile = (props) => (
   <div>
     {/* SEO */}
     <Helmet>
-      {/* Changes title in html head to 'My Profile - Crate' */}
+      {/* Changes title in html head to 'My Profile - Crate' <--- */}
       <title>My Profile - Crate</title>
     </Helmet>
 
     {/* Top title bar */}
-    <Grid style={{ backgroundColor: grey }}>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H3 font="secondary">My profile</H3>
-      </GridCell>
+    <Grid style={{ backgroundColor: grey }}> {/* Flex container with grid styling */}
+      <GridCell style={{ padding: '2em', textAlign: 'center' }}> {/* Style component for children of Grid */}
+        <H3 font="secondary">My profile</H3> {/* custom h3 element with secondary styling */}
+      </GridCell> 
     </Grid>
 
     <Grid>
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4>
+        <H4 style={{ marginBottom: '0.5em' }}>{props.user.details.name}</H4> {/* Custom h4 element that renders the name from the details property from the user store via mapStateToProps */}
 
-        <p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p> {/* renders email from details property (an object) from user store via mapStateToProps*/}
 
-        <Link to={userRoutes.subscriptions.path}>
-          <Button theme="primary">Subscriptions</Button>
+        <Link to={userRoutes.subscriptions.path}> {/* Link element from router that routes to the path from subscriptions */}
+          <Button theme="primary">Subscriptions</Button> {/* Custom Button element with primary theme */}
         </Link>
 
-        <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
+        <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button> {/* Custom Button with primary styling, that creates logout action to clear the user state */}
       </GridCell>
     </Grid>
   </div>
 )
 
 // Component Properties
-Profile.propTypes = {
-  user: PropTypes.object.isRequired,
+Profile.propTypes = { // Proptypes to check that state and dispatch are properly received from the store <---
+  user: PropTypes.object.isRequired, 
   logout: PropTypes.func.isRequired
 }
 

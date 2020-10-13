a21678b366a39d6e5b60297a2632a329cf058524
Sienna Kopf <princess.kopf@gmail.com>
2020-10-13 10:40:05 -0600

    Add graphQL middleware to beforeALl block of test

--- code/api/src/modules/user/query.test.js
+++ code/api/src/modules/user/query.test.js
@@ -1,4 +1,22 @@
+import request from 'supertest';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from '../../setup/schema';
+
+const app = express();
+
 describe("user queries", () => {
+  beforeAll(() =>{
+    server = express();
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    );
+  });
+
   it("is true", () => {
     expect(true).toBe(true)
   })

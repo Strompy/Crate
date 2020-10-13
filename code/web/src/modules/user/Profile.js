5a2282e22d12cf33c37e081879d15d6d3b33bece
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-10-13 12:04:07 -0600

    Add photo and adjust styling
    Co-authored-by: Josh Sevy <56780211+JoshSevy@users.noreply.github.com>
    Co-authored-by: Rachel Williams <62770765+rwilliams659@users.noreply.github.com>

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -7,13 +7,13 @@ import { Link } from 'react-router-dom'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
-import { Tile } from '../../ui/image'
-import { H3, H4, H6 } from '../../ui/typography'
+import { H3, H4, H5, H6 } from '../../ui/typography'
 import Button from '../../ui/button'
 import { grey, grey2, secondary } from '../../ui/common/colors'
+import { level1 } from '../../ui/common/shadows'
 
 // App Imports
-import userRoutes from '../../setup/routes/user'
+import userRoutes from '../../setup/routes/user' 
 import { logout } from './api/actions'
 import { APP_URL } from '../../setup/config/env'
 
@@ -26,35 +26,67 @@ const Profile = (props) => (
     </Helmet>
 
     {/* Top title bar */}
-    <Grid style={{ backgroundColor: grey }}>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+    <Grid style={{ backgroundColor: grey }} justifyRight={true}>
+      <GridCell style={{ padding: '2em', textAlign: 'center', maxWidth: '20vw', marginRight: '24%' }}>
         <H3 font="secondary">My profile</H3>
       </GridCell>
-      <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+      
+      <GridCell style={{ padding: '2em', textAlign: 'center', maxWidth: '9vw' }}>
         <Button theme="secondary" onClick={props.logout} style={{ marginLeft: '1em' }}>Logout</Button>
       </GridCell>
     </Grid>
 
     <Grid>
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <img src={`${APP_URL}//images/stock/men/1.jpg`} style={{ marginBottom: '2em', height: '25em' }}/>
+        <img
+          src={`${APP_URL}//images/stock/men/4.jpg`}
+          style={{ 
+            marginBottom: '2em', 
+            height: '24em', 
+            boxShadow: level1, 
+            borderRadius: '10px'
+          }}
+        /> 
         <H4>{props.user.details.name}</H4>
       </GridCell>
+
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H4 font='secondary' style={{ marginBottom: '0.5em' }}>Account Details</H4>
+        <H4 font='secondary' style={{ marginBottom: '1.5em' }}>Account Details</H4>
+
         <H6 style={{ marginBottom: '0.5em' }}>Account email:</H6>
         <p style={{ color: grey2, marginBottom: '2em' }}>{props.user.details.email}</p>
+
         <H6 style={{ marginBottom: '0.5em' }}>Shipping Address:</H6>
-        <p style={{ color: grey2, marginBottom: '2em' }}> crate-team@gmail.com</p>
+        <p style={{ color: grey2, marginBottom: '2em' }}>
+          123 Vanilla St.<br/>Chocolate, CO<br/>80911
+        </p>
+
         <H6 style={{ marginBottom: '0.5em' }}>Bio:</H6>
-        <p style={{ color: grey2, marginBottom: '2em' }}>Here is our bio.</p>
-        <Button theme="primary" style={{ marginLeft: '1em' }}>Edit Profile Info</Button>
+        <p style={{ color: grey2, width: '60%', margin: '0 auto 2em' }}>
+          "Clothes are just accessories for my rollerblades, and I'm all about accessories."
+        </p>
+
+        <Button theme="primary">Edit Profile Info</Button>
       </GridCell>
+
       <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-        <H4 style={{ color: secondary, marginBottom: '1em' }}>Your next crate<br/>will ship on: January 1, 2020</H4>
-        <Button theme="primary" style={{ marginLeft: '1em' }}>Edit Ship Date</Button>
-        <img src={`${APP_URL}/images/crate.png`} style={{ marginBottom: '1em', height: '15em', display: 'block', margin: '2em auto 1.5em auto'}}/>
-        <Button theme="primary" style={{ marginLeft: '1em' }}>View Order History</Button>
+        <div 
+          style={{
+            backgroundColor: grey,
+            borderRadius: '10px',
+            padding: '20px',
+            width: '75%',
+            margin: '0 auto',
+            boxShadow: `inset -1px 1px 10px ${grey2}`
+          }}
+        >
+          <H5 style={{ color: 'black', marginBottom: '0.5em' }}>Your next crate will ship on:</H5>
+          <H6 style={{ color: secondary, marginBottom: '1em' }}>November 13, 2020</H6>
+          <Button theme="primary">Edit Ship Date</Button>
+        </div>
+
+        <img src={`${APP_URL}/images/crate.png`} style={{ height: '12em', display: 'block', margin: '2.4em auto 1em' }}/>
+        <Button theme="primary">View Order History</Button>
       </GridCell>
     </Grid>
   </div>

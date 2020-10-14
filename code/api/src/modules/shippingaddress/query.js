7d5d9510b2ed31b0d241e40b31d10a528f059ab6
Sienna Kopf <princess.kopf@gmail.com>
2020-10-13 19:29:05 -0600

    Git shipping address to show up in GraphQL
    Co-Authored-By: Michael Alex Lynch <53200401+mlynch5187@users.noreply.github.com>
    Co-Authored-By: Phillip Strom  <62966813+Strompy@users.noreply.github.com>

--- code/api/src/modules/shippingaddress/query.js
+++ code/api/src/modules/shippingaddress/query.js
@@ -3,22 +3,22 @@ import { GraphQLInt, GraphQLList } from 'graphql'
 
 // App Imports
 import ShippingAddressType from './types'
-import { getAll, getByShippingaddress, get } from './resolvers'
+import { getAll, get, getByUser } from './resolvers'
 
 // Shipping Address All
-export const shippingaddress = {
+export const shippingAddresses = {
   type: new GraphQLList(ShippingAddressType),
   resolve: getAll
 }
 
 // Shipping Address By user
-export const subscriptionsByShippingaddress = {
+export const subscriptionsByShippingAddress = {
   type: new GraphQLList(ShippingAddressType),
   resolve: getByUser
 }
 
 // Shipping Address By id
-export const shippingaddress = {
+export const shippingAddressById = {
   type: ShippingAddressType,
   args: {
     id: { type: GraphQLInt }

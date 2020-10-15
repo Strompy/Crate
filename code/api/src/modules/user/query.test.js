55fec96adb48c23505e26d03304891c9d69c16c8
Phillip Strom <phillip.strom@gmail.com>
2020-10-15 13:37:38 -0600

    Add test for finding user by id

--- code/api/src/modules/user/query.test.js
+++ code/api/src/modules/user/query.test.js
@@ -36,9 +36,21 @@ describe("user queries", () => {
       expect(response.body.data.users[1].name).toEqual('The User')
       expect(response.body.data.users[0].email).toEqual('admin@crate.com')
       expect(response.body.data.users[1].email).toEqual('user@crate.com')
+      expect(response.body.data.users[0].id).toEqual(undefined)
+      expect(response.body.data.users[1].id).toEqual(undefined)
   })
 
   // it returns a user by id
+  it('returns a user by id', async () => {
+    const response = await request(server)
+      .post('/')
+      // .send({ query: '{ user(id = 1) { name email } }' })
+      .send({ query: '{ user(id: 1) { id name email password role bio image street city state zip } }'})
+      .expect(200)
+
+    const expectedKeys = ['id', 'name', 'email', 'password', 'role', 'bio', 'image', 'street', 'city', 'state', 'zip']
+    expect(Object.keys(response.body.data.user)).toEqual(expectedKeys)
+  })
 
   // it logs in a user and returns a JWT
 

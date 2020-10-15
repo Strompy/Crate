db24b3a8529efb51572fcabe014614ee6178fbb4
Sienna Kopf <princess.kopf@gmail.com>
2020-10-14 18:23:34 -0600

    Add mutations.test.js file and add basic always passing test and setup

---
+++ code/api/src/modules/product/mutations.test.js
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from '../../setup/schema';
+
+const app = express();
+
+describe("product mutations", () => {
+  let server;
+
+  beforeAll(() => {
+    server = express();
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    );
+  });
+
+  it("is true", () => {
+    expect(true).toBe(true)
+  })
+})

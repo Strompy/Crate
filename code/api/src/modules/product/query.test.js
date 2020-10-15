3f30ec689c314786cbfcb5947ebd4b69b1c7b569
Sienna Kopf <princess.kopf@gmail.com>
2020-10-14 18:02:30 -0600

    configure test db and write test for querying all products

--- code/api/src/modules/product/query.test.js
+++ code/api/src/modules/product/query.test.js
@@ -1,4 +1,32 @@
-describe("user queries", () => {
+import request from 'supertest';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import schema from '../../setup/schema';
+
+const app = express();
+
+describe("product queries", () => {
+  let server;
+
+  beforeAll(() => {
+    server = express();
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    );
+  });
+
+  it("returns all products", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: '{products { id, name, description, slug, type, gender, image } }' })
+      .expect(200)
+    expect(response.body.data.products.length).toEqual(8)
+  })
+
   it("is true", () => {
     expect(true).toBe(true)
   })

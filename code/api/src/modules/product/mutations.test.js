81d2a81fb4c8187fefe7589e3df6aea207a878ab
Sienna Kopf <princess.kopf@gmail.com>
2020-10-14 18:42:42 -0600

    Attempt mutation that requires authorization

--- code/api/src/modules/product/mutations.test.js
+++ code/api/src/modules/product/mutations.test.js
@@ -19,6 +19,21 @@ describe("product mutations", () => {
     );
   });
 
+  it("creates a product", async () => {
+    const response1 = await request(server)
+      .post('/')
+      .send({ query: '{ userLogin(email: "admin@crate.com", password: "123456") { token }}' })
+      .expect(200)
+    const token = response1.body.data.userLogin.token
+    
+    const response2 = await request(server)
+      .post('/')
+      .set('Authorization', 'Bearer ' + token)
+      .send({ mutation: '{ productCreate(name: "bucket hat", description: "stylish hat that the kool kids wear", slug: "bucket-hat", type: 2, gender: 2, image: "bucket-hat.png") { id } }' })
+      .expect(200)
+    console.log(response2.body.data)
+  })
+
   it("is true", () => {
     expect(true).toBe(true)
   })

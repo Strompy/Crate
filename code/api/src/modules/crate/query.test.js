00240163a1e966128ea16083641e3f172d22e0bb
Alex Lynch <mlynch5187@gmail.com>
2020-10-17 10:30:32 -0600

    Add test for returning an individual crate

--- code/api/src/modules/crate/query.test.js
+++ code/api/src/modules/crate/query.test.js
@@ -1,18 +1,40 @@
 import request from 'supertest';
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
 import schema from '../../setup/schema';
 
-describe("crate queries", () => {
-  beforeAll(() =>{
+const app = express();
+
+describe("crates queries", () => {
+  let server;
+
+  beforeAll(() => {
     server = express();
     server.use(
       '/',
       graphqlHTTP({
         schema: schema,
-        graphiql:false
+        graphiql: false
       })
     );
   });
+
+  it("returns all crates", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: '{crates { name, description } }' })
+      .expect(200)
+    expect(response.body.data.crates.length).toEqual(6)
+  })
+
+  it("returns a crate", async () => {
+    const response = await request(server)
+      .post('/')
+      .send({ query: '{ crateById(crateId: 1) { name, description } }' })
+      .expect(200)
+    expect(response.body.data.productById.name).toEqual("Clothes for Men")
+    expect(response.body.data.productById.description).toEqual("A monthly supply of trendy clothes for men.")
+  })
 
   it("is true", () => {
     expect(true).toBe(true)

5c183f6533cef4273e52dce09e9cd6968a53dddd
Phillip Strom <phillip.strom@gmail.com>
2020-10-17 12:20:26 -0600

    Create CrateProduct resolvers
    Co-Authored-By: Sienna Kopf <sienna-kopf@users.noreply.github.com>
    Co-Authored-By: Michael Alex <mlynch5187@users.noreply.github.com>

---
+++ code/api/src/modules/crateProduct/resolvers.js
@@ -0,0 +1,72 @@
+// App Imports
+import models from '../../setup/models'
+
+// Get CrateProduct by ID
+export async function get(parentValue, { id }) {
+  return await models.CrateProduct.findOne({
+    where: { id },
+    include: [
+      { model: models.Crate, as: 'crate' },
+      { model: models.Product, as: 'product' },
+    ]
+  })
+}
+
+// Get CrateProduct by crate
+export async function getByCrate (parentValue, { crateId }) {
+    return await models.Subscription.findAll({
+      where: {
+        crateId: crateId
+      },
+      include: [
+        {model: models.Product, as: 'product'},
+        {model: models.Crate, as: 'crate'},
+      ]
+    })
+  }
+}
+
+// Get CrateProduct by product
+export async function getByProduct (parentValue, { productId }) {
+    return await models.Subscription.findAll({
+      where: {
+        productId: productId
+      },
+      include: [
+        {model: models.Product, as: 'product'},
+        {model: models.Crate, as: 'crate'},
+      ]
+    })
+  }
+}
+
+// Get all crateProducts
+export async function getAll() {
+  return await models.CrateProduct.findAll({
+    include: [
+      { model: models.Product, as: 'product' },
+      { model: models.Crate, as: 'crate' },
+    ]
+  })
+}
+
+// Create crateProduct
+export async function create(parentValue, { crateId, productId }, { auth }) {
+  if(auth.user && auth.user.role === params.user.roles.admin) {
+    return await models.CrateProduct.create({
+      crateId,
+      productId
+    })
+  } else {
+    throw new Error('Operation denied.')
+  }
+}
+
+// Delete subscription
+export async function remove(parentValue, { id }, { auth }) {
+  if(auth.user && auth.user.role === params.user.roles.admin) {
+    return await models.CrateProduct.destroy({where: {id}})
+  } else {
+    throw new Error('Operation denied.')
+  }
+}

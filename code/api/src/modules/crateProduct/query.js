1d3c8fa4d0df9c1d0c032c8330af325a483c6b74
Phillip Strom <phillip.strom@gmail.com>
2020-10-17 12:05:56 -0600

    Add crateProduct queries
    Co-Authored-By: Sienna Kopf <sienna-kopf@users.noreply.github.com>
    Co-Authored-By: Michael Alex <mlynch5187@users.noreply.github.com>

---
+++ code/api/src/modules/crateProduct/query.js
@@ -0,0 +1,33 @@
+// Imports
+import { GraphQLInt, GraphQLList } from 'graphql'
+
+// App Imports
+import CrateProductType from './types'
+import { getAll, getByCrate, getProduct, get } from './resolvers'
+
+// CrateProducts All
+export const crateProducts = {
+  type: new GraphQLList(CrateProductType),
+  resolve: getAll
+}
+
+// CrateProducts by crate
+export const crateProductsByCrate = {
+  type: new GraphQLList(CrateProductType),
+  resolve: getByCrate
+}
+
+// CrateProducts by product
+export const crateProductsByProduct = {
+  type: new GraphQLList(CrateProductType),
+  resolve: getByProduct
+}
+
+// CrateProduct By id
+export const crateProduct = {
+  type: CrateProductType,
+  args: {
+    id: { type: GraphQLInt }
+  },
+  resolve: get
+}

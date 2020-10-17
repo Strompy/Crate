c419629ea08676ac1930ce1337a9a5687d81c106
Alex Lynch <mlynch5187@gmail.com>
2020-10-17 09:49:07 -0600

    Set test environment in database.json

--- code/api/src/modules/crate/query.test.js
+++ code/api/src/modules/crate/query.test.js
@@ -1,4 +1,19 @@
-describe("user queries", () => {
+import request from 'supertest';
+import express from 'express';
+import schema from '../../setup/schema';
+
+describe("crate queries", () => {
+  beforeAll(() =>{
+    server = express();
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql:false
+      })
+    );
+  });
+
   it("is true", () => {
     expect(true).toBe(true)
   })
